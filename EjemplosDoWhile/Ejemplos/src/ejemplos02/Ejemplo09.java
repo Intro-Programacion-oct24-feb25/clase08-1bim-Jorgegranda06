dacc08bfd104af65fc201b6018457b7800b1c9b1
Jorge Granda <jorjorgeandgranda06@gmail.com>
2024-11-27 12:31:07 -0500

    Cambios

--- EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo09.java
+++ EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo09.java
@@ -35,12 +35,22 @@ public class Ejemplo09 {
         cadenaFinal = "Listado de Notas\n";
         
         do {
+            System.out.println("Ingrese el nombre del estudiante");
+            nombre = entrada.nextLine(); 
             
-            
+            System.out.println("Ingrese calificaciones, solo valor entero");
+            nota = entrada.nextInt(); 
+           
             // agrego valor al acumulador
             suma = suma + nota;
             // agrego una unidad al contador para luego sacar el promedio
             contador_calificaciones = contador_calificaciones + 1;
+            
+             // agrego valor a la cadena final
+            cadenaFinal = String.format("%sCalificación %d del estudiante %s\n",
+                    cadenaFinal,
+                    nota, 
+                    nombre.toLowerCase());
             
             
             System.out.println("Ingrese (-111) si desea salir del ciclo; "
